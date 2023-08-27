7ab82de9142ecf6e90a6bc605a3119adca604742
Sibusiso <codyntuli@gmail.com>
2023-08-27 20:48:05 +0200

    added fragments and the bottom navigation bar

--- app/src/main/java/com/example/avianwatch/RegisterActivity.kt
+++ app/src/main/java/com/example/avianwatch/RegisterActivity.kt
@@ -1,11 +1,43 @@
 package com.example.avianwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var btnSignUp: Button
+    private lateinit var btnSignIn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //initializing all the views
+        initView()
+
+        // Sign up user
+        btnSignUp.setOnClickListener {
+            val SignUpIntent = Intent(this,RegisterActivity::class.java)
+            startActivity(SignUpIntent)
+            // Perform input validation
+            //performSignUp()
+        }
+        //Sign in user
+        btnSignIn.setOnClickListener {
+            val SigninIntent = Intent(this,LoginActivity::class.java)
+            startActivity(SigninIntent)
+        }
+
+        //rendering the logo to the screen
+        val imgIcon : ImageView = findViewById(R.id.imgIcon)
+        val drawable = ContextCompat.getDrawable(this, R.mipmap.origami)
+        imgIcon.setImageDrawable(drawable)
+    }
+
+    private fun initView() {
+        btnSignUp = findViewById(R.id.btnSignUp)
+        btnSignIn = findViewById(R.id.btnSignIn)
     }
 }
